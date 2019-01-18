0fa7c1ca1a67368108417e0424169a62dae6f20b
Nic Schumann <nicolas_schumann@brown.edu>
2019-01-18 15:19:08 -0500

    [plugins] update redirection plugin

--- wp-content/plugins/redirection/redirection-version.php
+++ wp-content/plugins/redirection/redirection-version.php
@@ -1,5 +1,5 @@
 <?php
 
-define( 'REDIRECTION_VERSION', '3.7.1' );
-define( 'REDIRECTION_BUILD', '606dc824e45401f3bb8c33953817e578' );
+define( 'REDIRECTION_VERSION', '3.7.2' );
+define( 'REDIRECTION_BUILD', '1db9a83c192c4b52c68ec743e037ef1c' );
 define( 'REDIRECTION_MIN_WP', '4.5' );
