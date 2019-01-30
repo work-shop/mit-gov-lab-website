708dd371493ef2775aa94b41385690e298a499fd
Nic Schumann <nicolas_schumann@brown.edu>
2019-01-30 15:24:56 -0500

    [subscribe] adds subscribe link implementation

---
+++ wp-content/themes/custom/partials/subscribe_link.php
@@ -0,0 +1,15 @@
+<section id="subscribe" class="bg-white pt2 pb2" aside-exit>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-10 col-xs-offset-1">
+                <div class="row">
+                    <div class="col-sm-6 col-sm-offset-3 centered">
+                        <a class="bg-brand white padded bold subscribe-link" href="/newsletter">
+                            Subscribe to our newsletter
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
