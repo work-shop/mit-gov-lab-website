281294de93cd0d208ade0ecf2b6fdf4f995b3ec4
Nic Schumann <nicolas_schumann@brown.edu>
2019-01-30 15:18:44 -0500

    [alert] wires up the sitewide alert to wordpress.

--- wp-content/themes/custom/partials/sitewide_alert.php
+++ wp-content/themes/custom/partials/sitewide_alert.php
@@ -1,10 +1,15 @@
-
-<section id="alert-banner" class="">
-    <div class="container-fluid alert-wrapper">
-        <div class="row">
-            <div class="col-xs-12 centered">
-                <p class="alert-text">We're updating our web platform. Check back for new content in the spring.</p>
+<?php if ( get_field('show_sitewide_alert', 'option') ): ?>
+    <section id="alert-banner" class="">
+        <div class="container-fluid alert-wrapper">
+            <div class="row">
+                <div class="col-xs-12 centered">
+                    <?php if ( $link = get_field('sitewide_alert_link', 'option') ): ?>
+                        <a href="<?php echo $link['url']; ?>" class="alert-text"><?php echo get_field('sitewide_alert_message', 'option'); ?></a>
+                    <?php else: ?>
+                        <p class="alert-text"><?php echo get_field('sitewide_alert_message', 'option'); ?></p>
+                    <?php endif; ?>
+                </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+<?php endif; ?>
