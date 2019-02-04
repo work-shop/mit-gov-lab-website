34f1af342514a0a5012591b9151bde4d0bad18ff
Nic Schumann <nicolas_schumann@brown.edu>
2019-02-04 14:53:12 -0500

    [updates] sets updates order to reverse chronological by publication_date

--- wp-content/themes/custom/partials/updates/list.php
+++ wp-content/themes/custom/partials/updates/list.php
@@ -3,7 +3,15 @@
         <div class="row">
             <div class="col-xs-offset-1 col-xs-10 bg-white">
                 <div id="news-list" class="row">
-                    <?php $updates = get_posts(array('post_type' => 'updates', 'posts_per_page' => -1)); ?>
+
+                    <?php $updates = get_posts(array(
+                        'post_type' => 'updates',
+                        'posts_per_page' => -1,
+                        'meta_key' => 'publication_date',
+                        'orderby' => 'meta_value',
+                        'order' => 'DESC'
+                    )); ?>
+
 
                     <?php global $post; ?>
                     <?php foreach ($updates as $post): ?>
