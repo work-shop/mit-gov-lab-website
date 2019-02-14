52d5422ec1c6fed23ca5347d7b2330eaff3b6a48
Nic Schumann <nicolas_schumann@brown.edu>
2019-02-14 10:50:05 -0500

    [consult] adds real data to the map.

--- wp-content/themes/custom/partials/map.php
+++ wp-content/themes/custom/partials/map.php
@@ -1,69 +1,65 @@
 <section class="block vh60 bg-light" id="map">
 
 
-	<?php 
+	<?php
 	$args = array(
 		'posts_per_page'        => -1,
 		'post_type'             => 'research'
 	);
 	$research_query = new WP_Query( $args );
 	$count = 0;
-	$mapOptions = array();
+	$mapOptions = array( 'data' => array() );
 
 	while ( $research_query->have_posts() ) : $research_query->the_post(); ?>
-		<?php 
-		
+		<?php
+
+        // Set up the Appropriate JSON structure for working
+        // with the map. DONT add HTML Strings here - json_encode
+        // does not handle html well, and will do annoying escaping
+        // that we don't want. We'll generate the HTML structure we
+        // want in JS on the client side.
+
 		$location = get_field('location');
 		$title = get_the_title();
 		$summary = get_field('summary');
 		$id = 'marker-' . get_the_ID();
 
-		$content = '<div class="marker-card">
-		<p class="marker-card-summary">'
-		. $summary .
-		'</p>
-		</div>';
+        if ( $location ) {
 
-		//mapOptions JSON Object goes here
+            $researchObject = array(
+                'marker' => array(
+                    'title' => $title,
+                    'position' => $location,
+                    'popup' => array(
+                        'id' => $id,
+                        'summary' => $summary
+
+                    )
+                )
+            );
+
+            $mapOptions['data'][] = $researchObject;
+
+        }
+
 		?>
 	<?php endwhile; ?>
 	<?php wp_reset_postdata(); ?>
 
 	<script>
-      // `mapOptions` extends options defined in the initializer.
-      var mapOptions = {
-      	data: [
-      	{
-      		"marker": {
-      			"title": "js",
-      			"position": {
-      				"lat": 41.81629200385342,
-      				"lng": -71.40854704864358
-      			},
-      			"popup": {
-      				"id": "-abc",
-      				"content": "<div>\n          <p>js</p>\n          <p><p>Olneyville Library is located at 1 Olneyville Square, <span class=\"locality\">Providence</span>, <span class=\"state\">RI</span> <span class=\"postal-code\">02909</span></p>\n</p>\n        </div>"
-      			}
-      		}
-      	},
-      	{
-      		"marker": {
-      			"title": "Knight Memorial Library, Elmwood Ave, Providence, RI, USA",
-      			"position": {
-      				"lat": 41.80505319999999,
-      				"lng": -71.42598420000002
-      			},
-      			"popup": {
-      				"id": "-xyz",
-      				"content": "<div>\n          <p>Knight Memorial Library, Elmwood Ave, Providence, RI, USA</p>\n          <p><p>Knight Memorial Library is located at 275 Elmwood Avenue, <span class=\"locality\">Providence</span>, <span class=\"state\">RI</span> <span class=\"postal-code\">02907</span></p>\n</p>\n        </div>"
-      			}
-      		}
-      	}
-      	],
-      	render: {
-      		zoom: 3,
-      	}
-      };
+        var mapOptions = <?php echo json_encode( $mapOptions, JSON_UNESCAPED_SLASHES ); ?>
+
+        // Okay, we got the data. Now we just need to build the html, and parse
+        // the latitude and longitude as integers.
+        mapOptions.data.forEach( function( research ) {
+            research.marker.position.lat = parseInt(research.marker.position.lat)
+            research.marker.position.lng = parseInt(research.marker.position.lng);
+            research.marker.popup.content = '<div class="marker-card"><p class="marker-card-summary">' + research.marker.popup.summary + '</p></div>';
+        });
+
+        // Log it out for verification.
+        console.log( mapOptions );
+
   </script>
   <div class="ws-map" data-options="mapOptions"></div>
 </section>
