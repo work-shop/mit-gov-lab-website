624f76d11ffda4072e6a0be42bc49af161090420
Nic Schumann <nicolas_schumann@brown.edu>
2019-03-06 11:55:56 -0500

    [bugfix] fixed a permalinks issue on related card.

--- wp-content/themes/custom/partials/updates/related_card.php
+++ wp-content/themes/custom/partials/updates/related_card.php
@@ -1,7 +1,7 @@
 <?php $hero = get_field('hero_image'); ?>
 
 <li class="p0 col-xs-offset-1 col-xs-10 project-result search-result mb1">
-    <a href="<?php get_the_permalink(); ?>" class="">
+    <a href="<?php echo get_the_permalink(); ?>" class="">
         <div class="col-sm-2 hidden-xs related-image-block-container">
             <div class="related-image-block" style="background-image:url('<?php echo $hero['sizes']['sm_square']; ?>');"></div>
         </div>
